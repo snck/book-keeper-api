4e279dc8c8a5be5fa18990bc4162396e6be39073
Nuttarat <muzcads@gmail.com>
2026-03-23 16:01:19 +0700

    Update migration file to add temporary solution for user's id issue

---
+++ migrations/000004_seed_users.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (id, user_name, created_at, updated_at)
+VALUES ('00000000-0000-0000-0000-000000000001', 'seed_user', NOW(), NULL);
---
+++ migrations/000005_seed_categories.up.sql
@@ -0,0 +1,7 @@
+INSERT INTO categories (category_name, user_id, created_at, updated_at)
+VALUES
+    ('Food', '00000000-0000-0000-0000-000000000001', NOW(), NULL),
+    ('Commute', '00000000-0000-0000-0000-000000000001', NOW(), NULL),
+    ('Health', '00000000-0000-0000-0000-000000000001', NOW(), NULL),
+    ('Shopping', '00000000-0000-0000-0000-000000000001', NOW(), NULL),
+    ('Entertainment', '00000000-0000-0000-0000-000000000001', NOW(), NULL);
